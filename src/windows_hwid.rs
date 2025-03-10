f94c585961253ac0502f79d0a606f2c4d23c3f19
Yohakim B. Samponu <mikahoy045@gmail.com>
2025-03-10 11:51:47 +0800

    Improve Windows WMI query reliability and performance
    - Add timeout for WMI queries to prevent potential hangs
    - Use explicit flags for query execution (FORWARD_ONLY, RETURN_IMMEDIATELY)
    - Refactor WMI connection parameters for more robust initialization
    - Optimize serial number and fallback information retrieval

--- src/windows_hwid.rs
+++ src/windows_hwid.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use windows::Win32::System::Com::{CoCreateInstance, CoInitializeEx, CLSCTX_INPROC_SERVER, COINIT_MULTITHREADED};
-use windows::Win32::System::Wmi::{IWbemLocator, WbemLocator};
-use windows::core::PCWSTR;
+use windows::Win32::System::Wmi::{IWbemLocator, WbemLocator, WBEM_FLAG_FORWARD_ONLY, WBEM_FLAG_RETURN_IMMEDIATELY};
+use windows::core::{BSTR, PCWSTR};
 
 pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
     // Initialize COM
@@ -15,28 +15,33 @@ pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
     };
 
     // Connect to WMI
-    let namespace = windows::core::BSTR::from("ROOT\\CIMV2");
-    let server = windows::core::BSTR::from("");
+    let namespace = BSTR::from("ROOT\\CIMV2");
+    let server = BSTR::from("");
+    let username = BSTR::from("");
+    let password = BSTR::from("");
+    let locale = BSTR::from("");
+    let authority = BSTR::from("");
     let services = unsafe {
         locator.ConnectServer(
             &namespace,
             &server,
-            PCWSTR::null(),
-            PCWSTR::null(),
-            None,
-            PCWSTR::null(),
-            None,
+            &username,
+            &password,
+            0,
+            &locale,
+            &authority,
         )?
     };
 
     // Execute query
-    let language = windows::core::BSTR::from("WQL");
-    let query = windows::core::BSTR::from("SELECT SerialNumber FROM Win32_BaseBoard");
+    let language = BSTR::from("WQL");
+    let query = BSTR::from("SELECT SerialNumber FROM Win32_BaseBoard");
+    let flags = WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY;
     let enumerator = unsafe {
         services.ExecQuery(
             &language,
             &query,
-            None,
+            flags,
             None,
         )?
     };
@@ -50,7 +55,7 @@ pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
         
         let result = unsafe { 
             enumerator.Next(
-                windows::Win32::Foundation::INFINITE,
+                5000, // 5 second timeout instead of INFINITE
                 std::mem::transmute(&mut item), 
                 &mut returned,
             )
@@ -61,7 +66,7 @@ pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
         }
 
         if let Some(object) = item {
-            let variant = unsafe { object.Get(windows::core::PCWSTR::from_raw(windows::core::w!("SerialNumber").as_ptr()))? };
+            let variant = unsafe { object.Get(PCWSTR::from_raw(windows::core::w!("SerialNumber").as_ptr()))? };
             if let Some(serial) = variant.to_string() {
                 mb_serial = serial;
                 break;
@@ -72,12 +77,12 @@ pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
     // Fallback to other identifiers if serial number is empty
     if mb_serial.trim().is_empty() {
         // Try to get the motherboard manufacturer + product instead
-        let query = windows::core::BSTR::from("SELECT Manufacturer, Product FROM Win32_BaseBoard");
+        let query = BSTR::from("SELECT Manufacturer, Product FROM Win32_BaseBoard");
         let enumerator = unsafe {
             services.ExecQuery(
                 &language,
                 &query,
-                None,
+                flags,
                 None,
             )?
         };
@@ -88,7 +93,7 @@ pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
             
             let result = unsafe { 
                 enumerator.Next(
-                    windows::Win32::Foundation::INFINITE,
+                    5000, // 5 second timeout instead of INFINITE
                     std::mem::transmute(&mut item), 
                     &mut returned,
                 )
@@ -99,8 +104,8 @@ pub fn get_motherboard_info() -> Result<String, Box<dyn Error>> {
             }
 
             if let Some(object) = item {
-                let mfr_variant = unsafe { object.Get(windows::core::PCWSTR::from_raw(windows::core::w!("Manufacturer").as_ptr()))? };
-                let prod_variant = unsafe { object.Get(windows::core::PCWSTR::from_raw(windows::core::w!("Product").as_ptr()))? };
+                let mfr_variant = unsafe { object.Get(PCWSTR::from_raw(windows::core::w!("Manufacturer").as_ptr()))? };
+                let prod_variant = unsafe { object.Get(PCWSTR::from_raw(windows::core::w!("Product").as_ptr()))? };
                 
                 let manufacturer = mfr_variant.to_string().unwrap_or_default();
                 let product = prod_variant.to_string().unwrap_or_default();
